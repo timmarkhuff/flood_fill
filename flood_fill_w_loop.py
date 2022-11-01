80bec63fce959842e45e2ff6ce13a04bc78d17f0
timmarkhuff <89954856+timmarkhuff@users.noreply.github.com>
2022-11-01 10:44:47 -0700

    Add files via upload

--- flood_fill_w_loop.py
+++ flood_fill_w_loop.py
@@ -25,6 +25,18 @@ class FloodFiller:
         self.fill_color = np.array([0,0,255])
         self.iteration = 0 # for adjusting the fill color
 
+        # define the relative positions of the nearby pixels to check
+        self.positions_to_check = [
+            [-1,0], # left
+            # [-1,1], # upper left
+            [0,1],  # above
+            # [1,1],  # upper right
+            [1,0],  # right
+            # [1,-1],  # lower right
+            [0,-1], # below
+            # [-1,-1], # lower left
+        ]
+
     def check_tolerance(self, x: int, y: int) -> bool:
         """
         Checks a specified pixel and determines if it is within acceptable 
@@ -45,6 +57,15 @@ class FloodFiller:
         y: y coordinate...
         """
         self.img_copy[y][x] = self.fill_color
+
+    def record_filled_pixel(self, x: int, y: int) -> None:
+        """
+        Updates self.pixel_fill_matrix so that we can check if the
+        pixel has been filled previously. 
+        x: x coordinate of the pixel
+        y: y coordinate...
+        
+        """
         self.pixel_fill_matrix[y][x] = 1
 
     def run(self):
@@ -54,17 +75,6 @@ class FloodFiller:
         and is_finished (bool).
         When is_finished = True, the image has been completed filled.
         """
-        # define the relative positions of the nearby pixels to check
-        positions_to_check = [
-            [-1,0], # left
-            # [-1,1], # upper left
-            [0,1],  # above
-            # [1,1],  # upper right
-            [1,0],  # right
-            # [1,-1],  # lower right
-            [0,-1], # below
-            # [-1,-1], # lower left
-        ]
 
         # EXPERIMENTING WITH DIFFERENT COLOR SCHEMES
         red = self.fill_color[2]
@@ -78,42 +88,40 @@ class FloodFiller:
         else:
             green = 255
             blue = 0
-
         self.fill_color = np.array([blue,green,red])
+        
+        fuel = 500
+        while len(self.pixels_to_fill) > 0 and fuel > 0:
+            fuel -= 1
+            pixel = self.pixels_to_fill[0] # queue 
+            self.pixels_to_fill = self.pixels_to_fill[1:]
+            # pixel = self.pixels_to_fill[-1] # stack
+            # self.pixels_to_fill = self.pixels_to_fill[:-1]
 
-        # update the sets
-        self.pixels_to_fill_current = self.pixels_to_fill_next.copy()
-        self.pixels_to_fill_next = set([])
-
-        for pixel in self.pixels_to_fill_current:
             # fill the pixel
             self.fill_pixel(pixel[0], pixel[1])
 
             # check which nearby pixels need to be filled
-            for position in positions_to_check:
+            for position in self.positions_to_check:
                 x_coord = pixel[0] + position[0]
                 y_coord = pixel[1] + position[1]
-
+                
                 # check if the new position is out of bounds
                 if x_coord > (self.img.shape[1] - 1) or x_coord < 0:
                     continue
                 if y_coord > (self.img.shape[0] - 1) or y_coord < 0:
                     continue
 
-                # check if the pixel is already filled
+                # check if the pixel has already been filled 
                 if self.pixel_fill_matrix[y_coord][x_coord]:
-                    # do not fill the pixel if it has already been filled
                     continue
-                
-                # check if the pixel is within tolerance
-                is_with_tolerance = self.check_tolerance(x_coord, y_coord)
-                if is_with_tolerance:
-                    self.pixels_to_fill_next.add((x_coord, y_coord))
 
-        if self.pixels_to_fill_next:
-            is_finished = False
-        else:
-            is_finished = True
+                # check if the pixel is within tolerance
+                if self.check_tolerance(x_coord, y_coord):
+                    self.pixels_to_fill.append((x_coord, y_coord))
+                    self.record_filled_pixel(x_coord, y_coord)
+                    
+        is_finished = len(self.pixels_to_fill) == 0
 
         self.iteration += 1
 
@@ -123,9 +131,8 @@ class FloodFiller:
         """
         Sets the (x, y) coordinates from which the flood fill will occur
         """
-        # initialize the lists for keeping track of pixels to check
-        self.pixels_to_fill_current = set([])
-        self.pixels_to_fill_next = set([(x, y)])
+        # initialize the list for keeping track of pixels to check
+        self.pixels_to_fill = [(x,y)]
 
         # set the hue range
         self.hue_max = self.hsv_img[y][x][0] + self.tolerance
@@ -133,7 +140,6 @@ class FloodFiller:
 
 def main():
     from timeit import default_timer as timer
-    from time import sleep
     import random
 
     # constants
